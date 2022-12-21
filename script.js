7851e03b0ab597381ea81334c29a061fae53290f
Christian Laettner Garcia <chrislae.garcia@gmail.com>
2022-12-21 15:00:36 +0800

    Add edit grid button with functionality

--- script.js
+++ script.js
@@ -2,39 +2,49 @@ const workspace = document.querySelector('.workspace');
 const btnColor = document.querySelector('#btn-color');
 const btnEraser = document.querySelector('#btn-eraser');
 
-let gridSize = 16;
+function drawGrid(gridSize) {
 
-for (y = 0; y < gridSize; y++) {
+  for (y = 0; y < gridSize; y++) {
 
-  const gridY = document.createElement('div');
+    const gridY = document.createElement('div');
 
-  for (x = 0; x < gridSize; x++) {
+    for (x = 0; x < gridSize; x++) {
 
-    const gridX = document.createElement('div');
-    gridX.classList.add('grid-box');
-    gridX.textContent = x;
+      const gridX = document.createElement('div');
+      gridX.classList.add('grid-box');
 
-    gridY.appendChild(gridX);
+      gridY.appendChild(gridX);
+    }
+
+    workspace.appendChild(gridY);
   }
-
-  workspace.appendChild(gridY);
 }
-
 let colorMode = true;
 
 document.addEventListener('click', (e) => {
   if (e.target.tagName != 'BUTTON') return;
 
+  colorMode = true;
   switch (e.target.textContent) {
-    case 'Color':
-      colorMode = true;
-      break;
     case 'Eraser':
       colorMode = false;
       break;
     case 'Clear':
       clearGrid();
-      colorMode = true;
+      break;
+    case 'Edit Grid':
+      let gridSize = prompt('Set Grid Size (2-100)');
+      
+      let child = workspace.lastElementChild
+
+      while (child) {
+        workspace.removeChild(child);
+        child = workspace.lastElementChild;
+      }
+
+      drawGrid(gridSize);
+      clearGrid();
+      break;
     default:
       return;
   }
@@ -58,3 +68,5 @@ document.addEventListener('mouseover', (e) => {
   else
     e.target.classList.remove('color-box');
 });
+
+drawGrid(16);
