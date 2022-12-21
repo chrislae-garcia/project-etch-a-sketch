8c331f26e27bfcc3f359aef050a146cbaa1c9b89
Christian Laettner Garcia <chrislae.garcia@gmail.com>
2022-12-21 13:37:44 +0800

    Add functionily to color and eraser buttons

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const workspace = document.querySelector('.workspace');
+const btnColor = document.querySelector('#btn-color');
+const btnEraser = document.querySelector('#btn-eraser');
 
 let gridSize = 16;
 
 for (y = 0; y < gridSize; y++) {
-  
+
   const gridY = document.createElement('div');
-  
+
   for (x = 0; x < gridSize; x++) {
-    
+
     const gridX = document.createElement('div');
     gridX.classList.add('grid-box');
     gridX.textContent = x;
@@ -18,9 +20,29 @@ for (y = 0; y < gridSize; y++) {
   workspace.appendChild(gridY);
 }
 
+let colorMode = true;
+
+document.addEventListener('click', (e) => {
+  if (e.target.tagName != 'BUTTON') return;
+
+  switch (e.target.textContent) {
+    case 'Color':
+      colorMode = true;
+      break;
+    case 'Eraser':
+      colorMode = false;
+      break;
+    default:
+      return;
+  }
+});
+
 document.addEventListener('mouseover', (e) => {
   if (!e.target.classList.contains('grid-box')) return;
   console.log(e.target.classList.contains('grid-box'));
 
-  e.target.classList.add('color-box');
-})
+  if (colorMode)
+    e.target.classList.add('color-box');
+  else
+    e.target.classList.remove('color-box');
+});
